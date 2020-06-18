0f7731d546e518f2cd28cb5b4e3d4a7993d4221a
Carlos Ortiz <cartex10@bu.edu>
2020-06-17 20:05:21 -0400

    alpha v1.1 release 5/16/20
    fix roll command
    separate testing bot and actual bot
    	*this way, I can test a future version of the bot while the other one runs without clogging up the server
    edit on_ready command to send as one message to not clog up notifications
    set base activity
    add changelog command
    add help strings to previous commands

--- rotomBot.py
+++ rotomBot.py
@@ -7,27 +7,36 @@ import random
 import math
 
 load_dotenv()
-#TOKEN = os.getenv('DISCORD_TOKEN')
+#TOKEN = os.getenv('DISCORD_TOKEN')		#Actual token
+#TOKEN = os.getenv('TEST_TOKEN')		#Test token
 #guild_id = os.getenv('GUILD_ID') #Actual server
 #guild_id = os.getenv('TEST_ID') #Test server
 
-bot = commands.Bot(command_prefix="~", status="dnd")
+base_activity = discord.Game(name="the ~help waiting game")
 
-@bot.command()
-async def repeat(ctx, *, inp):		#test function that repeats the input 
+bot = commands.Bot(command_prefix="~", status="dnd", activity=base_activity)
+
+@bot.command(help="Repeat any phrase", usage="[input]")
+async def repeat(ctx, *, inp):			#test function that repeats the input
 	await ctx.send(inp)
 
-@bot.command()
-async def roll(ctx, *inp):			#d20 roll function
-	roll = random.random() % 20 + 1
+@bot.command(help="Roll a d20", usage="[n/a (for now ;) )]")
+async def roll(ctx, *inp):				#d20 roll function
+	roll = random.randint(1, 20)
 	await ctx.send(roll)
 
-@bot.event							#called at bot startup
-async def on_ready():
+@bot.event							
+async def on_ready():					#called at bot startup
 	guild = bot.get_guild(guild_id)
 	chan = discord.utils.get(guild.text_channels, name="general")
-	await chan.send("ACTIVATING ROTOM BOT FOR TESTING")
-	await chan.send("HOPEFULLY THIS WORKS")
-	await chan.send("VERSION a1.0 LOADED")
+	await bot.change_presence(activity=base_activity, status="dnd")
+	await chan.send("ACTIVATING ROTOM BOT FOR TESTING\nHOPEFULLY THIS WORKS\nVERSION a1.1 LOADED")
+
+@bot.command(help="View full changelog", usage=" ")		
+async def changelog(ctx, *, inp):		#command to view full changelog
+	chlg = "```"
+	chlg += open("changelog.txt").read()
+	chlg += " ```"
+	await ctx.send(chlg)
 
 bot.run(TOKEN)
