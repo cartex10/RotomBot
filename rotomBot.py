245f3c1e4357316ef0c0b7792edcd90448a29d86
Carlos Ortiz <cartex10@bu.edu>
2020-08-25 19:51:27 -0400

    small fix

--- rotomBot.py
+++ rotomBot.py
@@ -9,9 +9,9 @@ import math
 
 load_dotenv()
 #TOKEN = os.getenv('DISCORD_TOKEN')		#Actual bot token
-TOKEN = os.getenv('TEST_TOKEN')		#Test bot token
+#TOKEN = os.getenv('TEST_TOKEN')		#Test bot token
 #guild_id = int(os.getenv('GUILD_ID')) #Actual server
-guild_id = int(os.getenv('TEST_ID')) #Test server
+#guild_id = int(os.getenv('TEST_ID')) #Test server
 
 #locked_roles = ["Admin", "fellowship", "dragonforce", "Groovy", "RotomBot", "@everyone", "BOTS"]
 locked_roles = ["CANNOT_ADD", "@everyone"]
